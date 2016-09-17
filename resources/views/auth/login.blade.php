db0aadaae73427a500294a9823d66c1f491924ac
MrPaulPhan <paul@mrpaulphan.com>
2016-09-17 17:47:36 -0400

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,33 +10,32 @@
     <div class="block__content">
         <form class="form" role="form" method="POST" action="{{ url('/login') }}">
         {{ csrf_field() }}
-
         <div class="form__group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="form__label">E-Mail Address</label>
-            <input id="email" type="email" class="form__input" name="email" value="{{ old('email') }}">
+            <label class="form__label" for="email">E-Mail Address</label>
+            <input class="form__input" id="email" type="email" name="email" value="{{ old('email') }}">
               @if ($errors->has('email'))
-                  <span class="alert">
-                      <strong>{{ $errors->first('email') }}</strong>
+                  <span class="block-alert">
+                      {{ $errors->first('email') }}
                   </span>
               @endif
-            </div>
+        </div>
 
         <div class="form__group{{ $errors->has('password') ? ' has-error' : '' }}">
             <label for="password" class="form__label">Password</label>
-                <input id="password" type="password" class="form__input" name="password">
-                @if ($errors->has('password'))
-                    <span class="alert">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
+            <input class="form__input" id="password" type="password" name="password">
+            @if ($errors->has('password'))
+                <span class="block-alert">
+                    {{ $errors->first('password') }}
+                </span>
+            @endif
         </div>
 
         <div class="form__group">
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="remember"> Remember Me
-                    </label>
-                </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="remember"> Remember Me
+                </label>
+            </div>
         </div>
 
         <div class="layout-split-2--apart">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,8 +12,8 @@
                     <label  class="form__label" for="name">Name</label>
                     <input class="form__input" id="name" type="text" name="name" value="{{ old('name') }}" autofocus>
                     @if ($errors->has('name'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('name') }}</strong>
+                        <span class="block-help">
+                            {{ $errors->first('name') }}
                         </span>
                     @endif
                 </div>
@@ -22,8 +22,8 @@
                     <label class="form__label" for="username" >Username</label>
                     <input class="form__input" id="username" type="text"  name="username" value="{{ old('username') }}" autofocus>
                     @if ($errors->has('username'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('username') }}</strong>
+                        <span class="block-help">
+                            {{ $errors->first('username') }}
                         </span>
                     @endif
                 </div>
@@ -32,8 +32,8 @@
                     <label class="form__label" for="email" >E-Mail Address</label>
                     <input class="form__input" id="email" type="email"  name="email" value="{{ old('email') }}">
                         @if ($errors->has('email'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
+                            <span class="block-help">
+                                {{ $errors->first('email') }}
                             </span>
                         @endif
                 </div>
@@ -42,8 +42,8 @@
                     <label class="form__label" for="password" >Password</label>
                     <input class="form__input" id="password" type="password"  name="password">
                     @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
+                        <span class="block-help">
+                            {{ $errors->first('password') }}
                         </span>
                     @endif
                 </div>
@@ -52,15 +52,18 @@
                     <label class="form__label" for="password-confirm" >Confirm Password</label>
                     <input class="form__input" id="password-confirm" type="password"  name="password_confirmation">
                     @if ($errors->has('password_confirmation'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                        <span class="block-help">
+                            {{ $errors->first('password_confirmation') }}
                         </span>
                     @endif
                 </div>
-                <div class="form__group">
-                        <button type="submit" class="button button--primary">
-                            Register
-                        </button>
+                <div class="layout-split-2--apart">
+                    <div class="column">
+                        <p><a href="{{ route('show.landing')}}" class="button--secondary">Cancel</a></p>
+                    </div>
+                    <div class="column">
+                        <button type="submit" class="button--primary">Register</button>
+                    </div>
                 </div>
             </form>
         </div>
